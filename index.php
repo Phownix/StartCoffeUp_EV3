00fd9bfd86a57e4eeb5d6658e180cfff32831085
Phownix <paulogutierrez211@gmail.com>
2023-06-28 14:02:39 -0400

    firts commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StartCoffeUp</title>
+    <link rel="stylesheet" href="/styles/main.css">
+</head>
+<body>
+    <div class="contact_nav">
+        <p>
+            <strong>Email:</strong>
+            <a href="mailto:contac@startcoffeup.cl">contac@startcoffeup.cl</a>
+        </p>
+        <p>
+            <strong>Numero:</strong>
+            <a href="tel:+56912345678">+569 12345678</a>
+        </p>
+        <p>
+            <strong>Direccion</strong>
+            <span>Calle Coffe #1213, Chillan</span>
+        </p>
+    </div>
+    <nav>
+        <h1 class="name_page">
+            StartCoffeUp
+        </h1>
+        <div class="menu">
+            <ul>
+                <li><a href="index.html#inicio">Inicio</a></li>
+                <li><a href="menu.html#cafe">Café</a></li>
+                <li><a href="menu.html#comida">Comida</a></li>
+                <li><a href="index.html#eventos">Eventos y torneos</a></li>
+                <li><a href="nosotros.html">Nosotros</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+            </ul>
+        </div>
+    </nav>
+    <main>
+
+    </main>
+
+    <footer>
+        Creado por un Inacapino <br>
+        &copy; StartCoffeUp 2023
+    </footer>
+</body>
+</html>
