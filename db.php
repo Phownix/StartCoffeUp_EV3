54785c9b50a396287647046cc222797b050229fd
Phownix <paulogutierrez211@gmail.com>
2023-06-29 10:35:23 -0400

    xd

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $servername = '127.0.0.1';
 $username = 'root';
 $password = '123456';
-$dbname = 'coff1e';
+$dbname = 'coffe';
 
 try {
     $conn = mysqli_connect($servername, $username, $password, $dbname);
